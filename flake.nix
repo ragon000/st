0c6293674c49d70d398154b55599c62c87773b85
Philipp Hochkamp <me@phochkamp.de>
2021-07-25 10:52:01 +0200

    flaky flake flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A flake for ragons st fork";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem(system: 
+  let pkgs = nixpkgs.legacyPackages.${system}; in
+  rec {
+
+    packages = flake-utils.lib.flattenTree {
+      st = pkgs.st.overrideAttrs ( oldAttrs: rec {
+        buildInputs = oldAttrs.buildInputs ++ [ pkgs.harfbuzz ];
+        src = ./.;
+      });
+    };
+    defaultPackage = packages.st;
+    apps.st = flake-utils.lib.mkApp { drv = packages.st; };
+    defaultApp = apps.st;
+  });
+}
